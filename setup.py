68452a43b27bbdf201d34c4a7c5983ed5e96decf
Daniel Engvall <daniel@engvalls.eu>
2021-11-15 19:42:26 +0100

    Adding installation requirements

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mail_relay',
+    version='0.0.1',
+    packages=['tests', 'mail_relay'],
+    install_requires=open('requirements.txt').read().split('\n'),
+    url='https://github.com/engdan77/mail_relay',
+    license='MIT',
+    author='Daniel Engvall',
+    author_email='daniel@engvalls.eu',
+    description='A simplistic SMTP relay service for Gmail and Home Assistant',
+    entry_points={
+        'console_scripts': ['mail_relay = mail_relay.__main__:main']
+        }
+)
