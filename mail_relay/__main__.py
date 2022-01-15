c2174d844eed675732197e02194d21a84b019541
Daniel Engvall <daniel@engvalls.eu>
2022-01-15 10:04:10 +0100

    Bumping to version 0.0.2

--- mail_relay/__main__.py
+++ mail_relay/__main__.py
@@ -1,7 +1,7 @@
 """mail_relay: A simplistic SMTP relay service for Gmail and Home Assistant"""
 
 __email__ = "daniel@engvalls.eu"
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 import os
 import signal
